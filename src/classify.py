7010ac21fbd61ef090fba792003b5af0f87a50b0
Rob Whitaker <rmw2@princeton.edu>
2016-02-17 11:27:11 -0500

    Improved classify.

--- src/classify.py
+++ src/classify.py
@@ -1,9 +1,8 @@
-import classify, vis, os, random, statistics
+import classify, vis, os, random
 import numpy as np
 import scipy.io as sio
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
-
 
 def test_model(data, model):
     genres = ['blues', 'classical', 'country', 'disco', 'hiphop', \
@@ -33,13 +32,13 @@ def int_as_genre(number):
     
     return genres[number-1]
 
-def grab_param(data, param, summarize_flag=True):
+def grab_param(data, param):
     # extract a given parameter name from the big array
 
     # since the vectors aren't all the same size, get min dimensions
     n_songs = len(data)
-    n_rows = min([data[i][param][0][0].shape[0] for i in range(n_songs)])
-    n_cols = min([data[i][param][0][0].shape[1] for i in range(n_songs)])
+    n_rows = data[0][param][0][0].shape[0]
+    n_cols = data[0][param][0][0].shape[1]
 
     # single element parameters are easy
     if n_cols == 1:
@@ -47,15 +46,31 @@ def grab_param(data, param, summarize_flag=True):
         grabbed = np.vstack(
             [data[i][param][0][0][0][0] for i in range(len(data))])
     elif n_rows == 12:
-        grabbed = np.empty((len(data), 0))
-        # use the size and loop through
-        if summarize_flag:
-            # select only the row corresponding to the key of the song
-            # get a matrix of features one row at a time
-            grabbed = np.vstack([np.array([
-                statistics.mean(data[i][param][0][0][data[i]['key'][0][0][0][0] - 1,0:n_cols]), 
-                statistics.stdev(data[i][param][0][0][data[i]['key'][0][0][0][0] - 1,0:n_cols])])
-                for i in range(n_songs)])
+        # select only the row corresponding to the key of the song
+        # get a matrix of features one row at a time
+        summaries = []
+        for i in range(n_songs):
+            # get key, only use info in relevant key
+            key = data[i]['key'][0][0][0][0]
+            # mean and std
+            mean = np.nanmean(data[i][param][0][0][key-1,:])
+            std = np.nanstd(data[i][param][0][0][key-1,:])
+            # add to running list
+            summaries.append(np.array([mean, std]))
+
+        grabbed = np.vstack(summaries)
+    elif n_rows == 24:
+        raise
+
+    else:
+        # simply summarize the row
+        summaries = []
+        for i in range(n_songs):
+            summaries.append(np.array(
+                [np.nanmean(data[i][param][0][0][0,:]), 
+                np.nanstd(data[i][param][0][0][0,:])]))
+
+        grabbed = np.vstack(summaries)
 
     return grabbed
 
@@ -65,8 +80,8 @@ def summarize(time_series):
     stats = np.zeros([n_rows, 2])
 
     for i in range(n_rows):
-        stats[i,0] = statistics.mean(time_series[i,:])
-        stats[i,1] = statistics.stdev(time_series[i,:])
+        stats[i,0] = np.nanmean(time_series[i,:])
+        stats[i,1] = np.nanstd(time_series[i,:])
 
     return stats
 
@@ -115,12 +130,14 @@ if __name__ == '__main__':
     # load all
     data = load_songs()
 
-    # Gaussian Naive Bayes on tempo and keystrength
-    p1, r1, gnb, m1 = general(['tempo', 'keystrength'], data)
+    # Gaussian Naive Bayes on tempo, keystrength, energy, inharmonicity
+    p1, r1, gnb, m1 = general(
+        ['tempo', 'keystrength', 'eng', 'inharmonic'], data)
     vis.present_results(p1,r1)
 
     # K-Nearest Neighbors on tempo and keystrength
-    p2, r2, knn, m2 = general(['tempo', 'keystrength'], data, 
+    p2, r2, knn, m2 = general(
+        ['tempo', 'keystrength', 'eng', 'inharmonic'], data, 
         classifier=KNeighborsClassifier)
     vis.present_results(p2,r2)
 
